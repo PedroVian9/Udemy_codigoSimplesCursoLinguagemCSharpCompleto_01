9e6e09fd8cb3e3cf9e424e6a64a2b627060e54ac
Pedro Macedo Paronetto Faria Viana <pedro.viana@gavresorts.com.br>
2023-09-14 15:36:12 -0300

    Adicionar arquivos de projeto.

---
+++ codigoSemPOOSimplesCursoUdemyLinguagemC#/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pX = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
+
+            double pY = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área é a de X");
+            }
+            else
+            {
+                if (areaY > areaX)
+                {
+                    Console.WriteLine("Maior área é a de Y");
+                }
+                else
+                {
+                    {
+                        Console.WriteLine("São iguais");
+                    }
+                }
+            }
+        }
+    }
+}
